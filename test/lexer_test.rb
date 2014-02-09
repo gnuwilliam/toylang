eed0930a704d800d220354431eee4e64a57fa15a
William Oliveira <sudowilliam@gmail.com>
2014-02-09 19:16:20 -0200

    test_string

--- test/lexer_test.rb
+++ test/lexer_test.rb
@@ -5,4 +5,8 @@ class LexerTest < Test::Unit::TestCase
   def test_number
     assert_equal [[:NUMBER, 1]], Lexer.new.tokenize("1")
   end
+
+  def test_string
+    assert_equal [[:STRING, "hi"]], Lexer.new.tokenize('"hi"')
+  end
 end
