06e02a8287d97e76cd503a601bc9b08fb25e0cce
William Oliveira <sudowilliam@gmail.com>
2014-02-10 04:32:28 -0200

    test_number

--- test/interpreter_test.rb
+++ test/interpreter_test.rb
@@ -1,3 +1,10 @@
 require "test_helper"
 require "interpreter"
 
+class InterpreterTest < Test::Unit::TestCase
+  def test_number
+    assert_equal 1, Interpreter.new.eval("1").ruby_value
+  end
+  
+  
+end
