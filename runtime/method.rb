82f55722a75ae03346a2f207a9b5f611f2026abb
William Oliveira <sudowilliam@gmail.com>
2014-02-10 02:48:58 -0200

    call method

--- runtime/method.rb
+++ runtime/method.rb
@@ -4,4 +4,14 @@ class ToyMethod
     @params = params
     @body = body
   end
+
+  def call(receiver, arguments)
+    # create a context of evaluation in which the method will execute
+    context = Context.new(receiver)
+
+    # assign arguments to local variables
+    @params.each_with_index do |param, index|
+      context.locals[param] = arguments[index]
+    end
+  end
 end
