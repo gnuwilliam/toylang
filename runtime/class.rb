d6680f2c7de2dc2b76be322f1f591291c6d869fb
William Oliveira <sudowilliam@gmail.com>
2014-02-10 02:42:49 -0200

    check if we're bootstraping (launching the runtime)

--- runtime/class.rb
+++ runtime/class.rb
@@ -13,5 +13,10 @@ class ToyClass < ToyObject
     # so we defer using those once the language is bootstrapped
     # this solves the chicken-or-the-egg problem with the Class class
     # we can initialize Class then set Class.class = Class
+    if defined?(Runtime)
+      runtime_class = Runtime["Class"]
+    else
+      runtime_class = nil
+    end
   end
 end
