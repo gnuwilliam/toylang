80d4045b0a9988962cb9d824d1884513ffe92c90
William Oliveira <sudowilliam@gmail.com>
2014-02-10 04:53:58 -0200

    typo

--- runtime/bootstrap.rb
+++ runtime/bootstrap.rb
@@ -1,10 +1,10 @@
 # boostrap the runtime
 # assemble all classes and objects together from the runtime
 
-toy_class = ToyClass.new              # Class
-toy_class.runtime_class = toy_class   # Class.class = Class
-object_class = ToyClass.new           # Object = Class.new
-object_class.runtime_class            # Object.class = Class
+toy_class = ToyClass.new                # Class
+toy_class.runtime_class = toy_class     # Class.class = Class
+object_class = ToyClass.new             # Object = Class.new
+object_class.runtime_class = toy_class  # Object.class = Class
 
 # create the Runtime object (the root context) on which all code starts its evaluation
 Runtime = Context.new(object_class.new)
