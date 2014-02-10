d1eb80bb9f681e5b62173e3640382c1234dc5332
William Oliveira <sudowilliam@gmail.com>
2014-02-10 02:35:22 -0200

    call method

--- runtime/object.rb
+++ runtime/object.rb
@@ -8,4 +8,10 @@ class ToyObject
     @runtime_class = runtime_class
     @ruby_value = ruby_value
   end
+
+  # call a method on the object
+  def call(method, arguments = [])
+    # like a typical class-based runtime model, store methods in the class of the object
+    @runtime_class.lookup(method).call(self, arguments)
+  end
 end
