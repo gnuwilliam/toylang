dfc3852cceb474fa02472267f3ced2a4e5263457
William Oliveira <sudowilliam@gmail.com>
2014-02-10 02:41:46 -0200

    initialize method

--- runtime/class.rb
+++ runtime/class.rb
@@ -2,4 +2,16 @@
 # inherit from ToyObject
 class ToyClass < ToyObject
   attr_reader = :runtime_methods
+
+  # creates a new class. number is an instance of class for example
+  def initialize
+    @runtime_methods = {}
+
+    # check if we're bootstraping (launching the runtime)
+    # during this process, the runtime is not fully initialized
+    # and core classes do not yet exists
+    # so we defer using those once the language is bootstrapped
+    # this solves the chicken-or-the-egg problem with the Class class
+    # we can initialize Class then set Class.class = Class
+  end
 end
