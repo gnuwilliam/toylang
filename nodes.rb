5dffde7e24375791bd8a38d728e7d2895d74b1e5
William Oliveira <sudowilliam@gmail.com>
2014-02-10 02:05:16 -0200

    true node

--- nodes.rb
+++ nodes.rb
@@ -11,3 +11,8 @@ end
 class LiteralNode < Struct.new(:value); end
 class NumberNode < LiteralNode; end
 class StringNode < LiteralNode; end
+class TrueNode < LiteralNode
+  def initialize
+    super(true)
+  end
+end
