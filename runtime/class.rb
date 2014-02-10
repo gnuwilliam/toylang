c7ac10698ebf197d9be86c27e9873a85ce6375a0
William Oliveira <sudowilliam@gmail.com>
2014-02-10 04:48:28 -0200

    fix typo

--- runtime/class.rb
+++ runtime/class.rb
@@ -1,7 +1,7 @@
 # represents a Toy class in the Ruby world. classes are objects in Toy so they
 # inherit from ToyObject
 class ToyClass < ToyObject
-  attr_reader = :runtime_methods
+  attr_reader :runtime_methods
 
   # creates a new class. number is an instance of class for example
   def initialize(superclass = nil)
