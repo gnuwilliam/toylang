15cf91477dfeb8934fc7b0cf5fd2daad077362aa
William Oliveira <sudowilliam@gmail.com>
2014-02-10 03:15:32 -0200

    create the Runtime object

--- runtime/bootstrap.rb
+++ runtime/bootstrap.rb
@@ -5,3 +5,6 @@ toy_class = ToyClass.new  				# Class
 toy_class.runtime_class = toy_class  	# Class.class = Class
 object_class = ToyClass.new 			# Object = Class.new
 object_class.runtime_class  			# Object.class = Class
+
+# create the Runtime object (the root context) on which all code starts its evaluation
+Runtime = Context.new(object_class.new)
