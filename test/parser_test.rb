59b96154bef14f1089cb40f7f9ddf48dfb624fa9
William Oliveira <sudowilliam@gmail.com>
2014-02-10 02:07:52 -0200

    test expressions

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -5,4 +5,10 @@ class ParserTest < Test::Unit::TestCase
   def test_number
     assert_equal Nodes.new([NumberNode.new(1)]), Parser.new.parse("1")
   end
+  
+  def test_expression
+    assert_equal Nodes.new([NumberNode.new(1), StringNode.new("hi")]), Parser.new.parse(%{1\n"hi"})
+  end
+  
+  
 end
