02ab1a67867577817fbc98705dafca80863bcc70
William Oliveira <sudowilliam@gmail.com>
2014-02-10 03:12:13 -0200

    toy_class.runtime_class

--- runtime/bootstrap.rb
+++ runtime/bootstrap.rb
@@ -1,4 +1,5 @@
 # boostrap the runtime
 # assemble all classes and objects together from the runtime
 
-toy_class = ToyClass.new  			# Class
+toy_class = ToyClass.new  				# Class
+toy_class.runtime_class = toy_class  	# Class.class = Class
