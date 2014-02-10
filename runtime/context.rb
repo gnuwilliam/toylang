eed06b9fe74a6641966b9b70115ec0c2be9be99a
William Oliveira <sudowilliam@gmail.com>
2014-02-10 03:01:03 -0200

    store constants

--- runtime/context.rb
+++ runtime/context.rb
@@ -1,4 +1,11 @@
 # the evaluation context
 class Context
   attr_reader :locals, :current_self, :current_class
+
+  # store constants as class variable
+  # class variables start with @@
+  # instance variables start with @ in Ruby since they are globally accessible
+  # implement namespacing of constants, you could store it in the instance of this class
+  @@constants = {}
+
 end
