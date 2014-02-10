66b4de5d70632bcf60a009bf7edabf3b026b0522
William Oliveira <sudowilliam@gmail.com>
2014-02-10 04:37:33 -0200

    test create an object

--- test/runtime_test.rb
+++ test/runtime_test.rb
@@ -6,5 +6,9 @@ class RuntimeTest < Test::Unit::TestCase
     assert_not_nil Runtime["Object"]
   end
   
+  def test_create_an_object
+    assert_equal Runtime["Object"], Runtime["Object"].new.runtime_class
+  end
+  
   
 end
