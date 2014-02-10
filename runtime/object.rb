de7c6037899fbbb29af4f699d3268ea6ec86b25a
William Oliveira <sudowilliam@gmail.com>
2014-02-10 02:34:24 -0200

    initialize method

--- runtime/object.rb
+++ runtime/object.rb
@@ -4,4 +4,8 @@ class ToyObject
 
   # each object have a class (named runtime_class to prevent errors with Ruby's class method)
   # optionaly an object can hold a Ruby value (eg.: numbers and strings)
+  def initialize(runtime_class, ruby_value = self)
+    @runtime_class = runtime_class
+    @ruby_value = ruby_value
+  end
 end
