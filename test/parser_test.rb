e5cdc729b0c2e3772357c69767a27ce67ea115df
William Oliveira <sudowilliam@gmail.com>
2014-02-10 02:20:46 -0200

    test assign

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -18,5 +18,10 @@ class ParserTest < Test::Unit::TestCase
     assert_equal Nodes.new([CallNode.new(nil, "method", [NumberNode.new(1), NumberNode.new(2)])]), Parser.new.parse("method(1, 2)")
   end
   
+  def test_assign
+    assert_equal Nodes.new([SetLocalNode.new("a", NumberNode.new(1))]), Parser.new.parse("a = 1")
+    assert_equal Nodes.new([SetConstantNode.new("A", NumberNode.new(1))]), Parser.new.parse("A = 1")
+  end
+  
   
 end
