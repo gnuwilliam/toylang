5fb2fa927e425134316dfdbc984ffa6b1d9ff50b
William Oliveira <sudowilliam@gmail.com>
2014-02-10 03:23:58 -0200

    add `new` method to classes

--- runtime/bootstrap.rb
+++ runtime/bootstrap.rb
@@ -1,10 +1,10 @@
 # boostrap the runtime
 # assemble all classes and objects together from the runtime
 
-toy_class = ToyClass.new  				# Class
-toy_class.runtime_class = toy_class  	# Class.class = Class
-object_class = ToyClass.new 			# Object = Class.new
-object_class.runtime_class  			# Object.class = Class
+toy_class = ToyClass.new              # Class
+toy_class.runtime_class = toy_class   # Class.class = Class
+object_class = ToyClass.new           # Object = Class.new
+object_class.runtime_class            # Object.class = Class
 
 # create the Runtime object (the root context) on which all code starts its evaluation
 Runtime = Context.new(object_class.new)
@@ -23,3 +23,9 @@ Runtime["NilClass"] = ToyClass.new
 Runtime["true"] = Runtime["TrueClass"].new_with_value(true)
 Runtime["false"] = Runtime["FalseClass"].new_with_value(true)
 Runtime["nil"] = Runtime["NilClass"].new_with_value(true)
+
+# add `new` method to classes, used to instantiate a class:
+# eg.: Object.new, Number.new, String.new
+Runtime["Class"].runtime_methods["new"] = proc do |receiver, arguments|
+  receiver.new
+end
