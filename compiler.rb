67bd4bd7d53a1fd6ccd005608bd8a74c127d8588
William Oliveira <sudowilliam@gmail.com>
2014-02-11 16:08:44 -0200

    initialize compiler class

--- compiler.rb
+++ compiler.rb
@@ -8,3 +8,9 @@ require 'llvm/transforms/scalar'
 require 'llvm/transforms/ipo'
 
 LLVM.init_x86
+
+# compiler is used in a similar way as the runtime
+# but instead of executing code, it will generate LLVM byte-code
+# for later execution
+class Compiler
+end
