ba1443a81df222baea6555ccb734b130ff347135
William Oliveira <sudowilliam@gmail.com>
2014-02-11 16:17:02 -0200

    initialize method

--- compiler.rb
+++ compiler.rb
@@ -16,4 +16,26 @@ class Compiler
   # initialize llvm types
   PCHAR = LLVM::Type.pointer(LLVM::Int8) # equivalent to *char in C
   INT   = LLVM::Int # equivalent to int in C
+
+  attr_reader :locals
+
+  def initialize(mod = nil, function = nil)
+    # create the LLVM module in which to store the code
+    @module = mod || LLVM::Module.create("toy")
+
+    # to track local names during compilation
+    @locals = {}
+
+    # function in which the code will be put
+    @function = function ||
+      # by default, create a main function as it's the std entry point
+      @module.functions.named("main") ||
+      @module.functions.add("main", [INT, LLVM::Type.pointer(PCHAR)], INT)
+
+    # create an LLVM byte-code builder
+    @builder = LLVM::Builder.create
+    @builder.position_at_end(@function.basic_blocks.append)
+
+    @engine = LLVM::ExecutionEngine.create_jit_compiler(@module)
+  end
 end
